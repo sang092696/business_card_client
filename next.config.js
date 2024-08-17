598f30534436030e6b3006763c8dddb389374ec6
sang092696 <61214673+sang092696@users.noreply.github.com>
2024-08-17 15:59:39 -0400

    set redirect to not cache

--- next.config.js
+++ next.config.js
@@ -9,6 +9,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/',
+        permanent: false,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
